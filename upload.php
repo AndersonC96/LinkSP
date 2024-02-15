0a4e2241b06d753094ad3bd4f84e14b7066ff84f
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-02-15 17:52:43 -0300

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,25 +1,22 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "link";
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    if($conn->connect_error){
-        die("Erro na conexão com o banco de dados: " . $conn->connect_error);
-    }
+    require_once "db.php";
     if($_FILES["file"]["error"] > 0){
         echo "Erro: " . $_FILES["file"]["error"] . "<br>";
     }else{
         $target_dir = "./Uploads/";
         $target_file = $target_dir . basename($_FILES["file"]["name"]);
-        move_uploaded_file($_FILES["file"]["tmp_name"], $target_file);
-        $file_name = $_FILES["file"]["name"];
-        $file_path = $target_file;
-        $sql = "INSERT INTO files (file_name, file_path) VALUES ('$file_name', '$file_path')";
-        if($conn->query($sql) === TRUE){
-            echo "Arquivo enviado com sucesso.";
+        if(move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)){
+            $file_name = $_FILES["file"]["name"];
+            $file_path = $target_file;
+            $sql = "INSERT INTO files (file_name, file_path) VALUES ('$file_name', '$file_path')";
+            if ($conn->query($sql) === TRUE){
+                header("Location: index.php?success=true");
+                exit();
+            }else{
+                echo "Erro ao enviar o arquivo: " . $conn->error;
+            }
         }else{
-            echo "Erro ao enviar o arquivo: " . $conn->error;
+            echo "Erro ao mover o arquivo para o diretório de upload.";
         }
     }
     $conn->close();
