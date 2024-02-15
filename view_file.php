b38652ad1dcbe52d734888374a3d1bc5d807f7f7
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-02-15 17:11:49 -0300

    Update view_file.php

--- view_file.php
+++ view_file.php
@@ -1,26 +1,25 @@
 <?php
-    if(isset($_GET['id']) && !empty($_GET['id'])){
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "link";
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        if($conn->connect_error){
-            die("Erro na conexão com o banco de dados: " . $conn->connect_error);
-        }
-        $id = $_GET['id'];
-        $sql = "SELECT * FROM files WHERE id=$id";
-        $result = $conn->query($sql);
-        if($result->num_rows > 0){
-            $row = $result->fetch_assoc();
-            $file_path = $row['file_path'];
-            echo "<h2>Link de compartilhamento:</h2>";
-            echo "<a href='$file_path' target='_blank'>Link</a>";
-        }else{
-            echo "Arquivo não encontrado.";
-        }
-        $conn->close();
-    }else{
-        echo "ID do arquivo não especificado.";
+if(isset($_GET['id']) && !empty($_GET['id'])){
+    require_once('db.php');
+    // Obtém o ID do arquivo
+    $id = $_GET['id'];
+
+    // Consulta o arquivo no banco de dados
+    $sql = "SELECT * FROM files WHERE id=$id";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        // Gera o link de compartilhamento
+        $file_path = $row['file_path'];
+        echo "<h2>Link de compartilhamento:</h2>";
+        echo "<a href='$file_path' target='_blank'>Link</a>";
+    } else {
+        echo "Arquivo não encontrado.";
     }
+
+    $conn->close();
+} else {
+    echo "ID do arquivo não especificado.";
+}
 ?>
