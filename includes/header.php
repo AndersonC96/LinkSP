07437dac6405ddd8907d6579fbd956556da3a96b
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-01-21 18:25:59 -0300

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,30 +1,34 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple Pharma - Upload de Arquivos</title>
+    <title>LinkSP - Upload de Arquivos</title>
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="/images/favicon.png">
     <!-- Link do CSS do Bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <!-- CSS Personalizado -->
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-    <header class="bg-primary text-white py-3">
+    <header>
         <div class="container">
-            <h1>Simple Pharma - Sistema de Upload</h1>
-            <nav class="navbar navbar-expand-lg navbar-dark">
-                <div class="container-fluid">
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <div class="d-flex justify-content-between align-items-center py-3">
+                <h1>LinkSP</h1>
+                <nav class="navbar navbar-expand-lg navbar-dark">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item"><a class="nav-link" href="/index.php">Home</a></li>
-                            <li class="nav-item"><a class="nav-link" href="/pages/upload.php">Upload</a></li>
-                            <li class="nav-item"><a class="nav-link" href="/pages/list_files.php">Arquivos</a></li>
+                        <ul class="navbar-nav ms-auto">
+                            <li class="nav-item"><a class="nav-link" href="./index.php">Home</a></li>
+                            <li class="nav-item"><a class="nav-link" href="./pages/upload.php">Upload</a></li>
+                            <li class="nav-item"><a class="nav-link" href="./pages/list_files.php">Arquivos</a></li>
                         </ul>
                     </div>
-                </div>
-            </nav>
+                </nav>
+            </div>
         </div>
     </header>
     <main class="container my-4">
