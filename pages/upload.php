de12db52719be7c20521d7dfe6d4d241f9ba5476
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-01-21 17:28:47 -0300

    Create upload.php

---
+++ pages/upload.php
@@ -0,0 +1,23 @@
+<?php
+    require_once "db.php";
+    if($_FILES["file"]["error"] > 0){
+        echo "Erro: " . $_FILES["file"]["error"] . "<br>";
+    }else{
+        $target_dir = "./Uploads/";
+        $target_file = $target_dir . basename($_FILES["file"]["name"]);
+        if(move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)){
+            $file_name = $_FILES["file"]["name"];
+            $file_path = $target_file;
+            $sql = "INSERT INTO files (file_name, file_path) VALUES ('$file_name', '$file_path')";
+            if ($conn->query($sql) === TRUE){
+                header("Location: index.php?success=true");
+                exit();
+            }else{
+                echo "Erro ao enviar o arquivo: " . $conn->error;
+            }
+        }else{
+            echo "Erro ao mover o arquivo para o diretório de upload.";
+        }
+    }
+    $conn->close();
+?>
