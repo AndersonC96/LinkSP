1df41da7b54532f23a41372ab1b298fde97a8f68
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-01-21 17:27:32 -0300

    Create db.php

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "link";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if($conn->connect_error){
+        die("Erro na conexão com o banco de dados: " . $conn->connect_error);
+    }
+?>
