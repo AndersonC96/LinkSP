1e66a714e44c1c76db23ab428c52882f58cb7bc0
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-01-21 18:25:46 -0300

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
         </main>
-        <footer class="bg-dark text-white text-center py-3">
-            <p>&copy; <?php echo date('Y'); ?> Simple Pharma. Todos os direitos reservados.</p>
+        <footer>
+            <p>&copy; <?php echo date('Y'); ?> LinkSP. Todos os direitos reservados.</p>
         </footer>
         <!-- Link do JS do Bootstrap -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
