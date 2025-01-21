ba3891162ff928d3350554dff8f1702700c21552
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-01-21 18:30:25 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <div class="container d-flex justify-content-center align-items-center" style="height: 70vh;">
     <div class="card shadow p-4" style="border-radius: 15px; width: 500px;">
         <h2 class="mb-4 text-center">Upload de Arquivo PDF</h2>
-        <form action="upload.php" method="post" enctype="multipart/form-data" class="text-center">
+        <form action="./pages/upload.php" method="post" enctype="multipart/form-data" class="text-center">
             <div class="custom-file mb-4">
                 <input type="file" class="custom-file-input" id="file" name="file" accept=".pdf" required>
                 <label class="custom-file-label" for="file" id="file-label">Selecione o arquivo PDF</label>
